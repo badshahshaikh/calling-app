205d9921effb9aa4ac8a9fa9b389fe6d1ef650f6
badshah <bmansoori@xperiolabs>
2025-01-04 17:29:08 +0530

    udpated for creating websocket server 4

--- createSocketServer.php
+++ createSocketServer.php
@@ -41,7 +41,7 @@ class MyWebSocket implements MessageComponentInterface {
 $loop = Factory::create();
 
 // Create the base React socket $webSocket = new Server('0.0.0.0:8000', $loop);
-$webSocket = new Server('3.124.177.12:443', $loop);
+$webSocket = new Server('0.0.0.0:8080', $loop);
 
 // Wrap the React socket in a SecureServer for SSL
 $secureWebSocket = new SecureServer($webSocket, $loop, [
